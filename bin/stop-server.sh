c9150cf767c62f70862c4df5dc17d895f3887c2e
Mushtaque <mushtaquethegeek@gmail.com>
2016-04-11 17:41:58 +0530

    fixed bug

--- bin/stop-server.sh
+++ bin/stop-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-SCREEN_PID="`ps -ef | grep node | head -n 1 | cut -d " " -f 2 | tr -d "\n"`"
-echo "SCREEN_PID is $SCREEN_PID"
+SCREEN_PID="`ps -ef | grep SCREEN | head -n 1 \
+	| cut -d " " -f 3 | tr -d "\n"`"
+
+echo "Killing screen with SCREEN_PID $SCREEN_PID"
 kill -9 $SCREEN_PID
